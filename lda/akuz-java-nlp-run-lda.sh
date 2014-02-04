bbbd671d7ed2d2e18f4b78265c8628ffcb40dd4e
Chris Tensmeyer <Christopher.tensmeyer@gmail.com>
2014-02-04 15:02:24 -0700

    changed shell script

--- lda/akuz-java-nlp-run-lda.sh
+++ lda/akuz-java-nlp-run-lda.sh
@@ -2,16 +2,16 @@
 
 echo START: akuz-java-nlp-run-lda...
 
-java -Xms256m -Xmx4g \
+java -Xms256m -Xmx16g \
         -Dfile.encoding=UTF-8 \
         -jar ./akuz-java-nlp-run-lda-0.0.2.jar \
-        -inputDir ../data/ \
-        -outputDir ./output \
+        -inputDir ../data/no_stem \
+        -outputDir ../output/lda_output \
         -topicsConfigFile ./topics_config.txt \
         -stopWordsFile ./stop_words.txt \
 	-burnInTempIter 50 \
 	-samplingIter 100 \
-        -threadCount 4 \
+        -threadCount 7 \
 
 if [ "$?" -ne "0" ]; then
   echo "ERROR: akuz-java-nlp-run-lda"
