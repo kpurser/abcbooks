ee4f8694e550e65f1a02e9ccca19c35e07a3ebd4
DrChole <soledadpera@gmail.com>
2014-02-05 11:14:24 -0700

    Added the test for getting words
    Getting the 26 words in output file

--- test/topic/Test.java
+++ test/topic/Test.java
@@ -1,19 +1,40 @@
 
 package topic;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class Test
 {
 	public static void main(String args[]) throws IOException
 	{
-		System.out.println(args);
 		String filename = args[0];
 		LDA_Parser parser = new LDA_Parser(filename);
+		
 		Topic[] topics = parser.parse();
+		
+		PorterStemmer stem= new PorterStemmer();
+	
 		for (Topic topic: topics)
 		{
+			
 			System.out.println(topic);
+		
 		}
+		
+		String matrix=args[1];
+		String inputTopic=args[2];
+		inputTopic=stem.stem(inputTopic); // stem input topic to match words
+		abcWords theWords=new abcWords(topics, inputTopic, matrix);
+		HashMap<String, String> chosenWords=theWords.get26Words();
+		
+		System.out.println("Selected 26 words ");
+		for(String w:chosenWords.keySet())
+			System.out.println(w + " " + chosenWords.get(w));
+
+
+
+		
+		
 	}
 }
 
