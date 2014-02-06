648e86998f6ec3d1ac7e7049c50dfe902c785f97
DrChole <soledadpera@gmail.com>
2014-02-05 17:48:11 -0700

    Update
    Now we get both lists of 26 words, and I guess we'll decide tomorrow
    which one we would like to use....

--- test/topic/Test.java
+++ test/topic/Test.java
@@ -10,7 +10,11 @@ public class Test
 		String filename = args[0];
 		LDA_Parser parser = new LDA_Parser(filename);
 		
+		String filename2 = args[2];
+		LDA_Parser parser2 = new LDA_Parser(filename2);
+		
 		Topic[] topics = parser.parse();
+		Topic[] topicsNS = parser2.parse();
 		
 		PorterStemmer stem= new PorterStemmer();
 	
@@ -22,20 +26,20 @@ public class Test
 		}
 		
 		String matrix=args[1];
-		String inputTopic=args[2];
+		String inputTopic=args[3];
 		String sinputTopic=stem.stem(inputTopic); // stem input topic to match words
 		abcWords theWords=new abcWords(topics, sinputTopic, matrix);
 		HashMap<String, String> chosenWords=theWords.get26Words();
 		
-		System.out.println("Selected 26 words ");
+		System.out.println("Selected 26 words based on stemmed topics and CorrelationMatrix");
 		for(String w:chosenWords.keySet())
 			System.out.println(w + " " + chosenWords.get(w));
 
 
-		abcWordsWN theWordsWN=new abcWordsWN(topics, sinputTopic, matrix);
+		abcWordsWN theWordsWN=new abcWordsWN(topicsNS, inputTopic, matrix);
 		HashMap<String, String> chosenWordsWN=theWordsWN.get26Words();
 		
-		System.out.println("Selected 26 words ");
+		System.out.println("Selected 26 words based on non-stemmed topics and WordNet ");
 		for(String w:chosenWordsWN.keySet())
 			System.out.println(w + " " + chosenWordsWN.get(w));
 		
